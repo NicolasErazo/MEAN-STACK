3dbda2f2fd5e915fbaa3b3fb9567cfd324847291
Nicolas Erazo <nico.lacho@outlook.com>
2023-04-04 17:32:57 -0500

    Update

--- src/routes/employees.routes.js
+++ src/routes/employees.routes.js
@@ -11,18 +11,3 @@ router.put('/employees/:id', employeesCtrl.editEmployee);
 router.delete('/employees/:id', employeesCtrl.deleteEmployee);
 
 module.exports = router;
-
-function verifyToken(req, res, next){
-    if(!req.headers.authorization){
-        return res.status(401).send('Not authorized');
-    }
-
-    const token = req.headers.authorization.split(" ")[1]
-    if(token == 'null'){
-        return res.status(401).send('Not authorized');
-    }
-
-    const data = jwt.verify(token, 'SeCrEtKeY')
-    req.userId = data._id
-    next();
-}
