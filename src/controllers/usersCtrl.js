b58029847a94f87a5e8d229ee4b6c0112357ab03
Nicolas Erazo <nico.lacho@outlook.com>
2023-04-05 14:42:17 -0500

    Upadate

--- src/controllers/usersCtrl.js
+++ src/controllers/usersCtrl.js
@@ -4,22 +4,22 @@ const jwt = require('jsonwebtoken');
 
 userCtrl.login = async (req, res) => {
     const { email, password } = req.body;
-    const userDates = await user.findOne({email})
+    const userDates = await user.findOne({ email })
 
-    if(!userDates) return res.status(401).send("Email doesn't exists");
-    if(userDates.password !== password) return res.status(401).send("Wrong password");
+    if (!userDates) return res.status(401).send("Email doesn't exists");
+    if (userDates.password !== password) return res.status(401).send("Wrong password");
 
-    const token = jwt.sign({_id: userDates._id}, 'SeCrEtKeY');
-    res.status(200).json({token});
+    const token = jwt.sign({ _id: userDates._id }, 'SeCrEtKeY');
+    res.status(200).json({ token });
 }
 
 userCtrl.register = async (req, res) => {
     const { name, username, email, password } = req.body;
     const newUser = new user({ name, username, email, password });
     await newUser.save();
-    
-    const token = jwt.sign({_id: newUser._id}, 'SeCrEtKeY');
-    res.status(200).json({token});
-}   
 
-module.exports = userCtrl   
+    const token = jwt.sign({ _id: newUser._id }, 'SeCrEtKeY');
+    res.status(200).json({ token });
+}
+
+module.exports = userCtrl
